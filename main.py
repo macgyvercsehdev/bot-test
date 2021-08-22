3f91e83ef0ad3dff65924245149163a46bc32f06
macgyvercseh-dev <macgyvercseh@outlook.com>
2021-08-22 02:29:41 -0300

    fix

--- main.py
+++ main.py
@@ -55,11 +55,11 @@ async def preco_slp(preco_slp):
     
     
     browser = webdriver.Chrome(options=options)
-
-    browser.get("https://www.tradingview.com/chart/?symbol=BITSTAMP%3ABTCUSD")
-
     browser.maximize_window()
-
+    browser.get("https://www.tradingview.com/chart/?symbol=BITSTAMP%3ABTCUSD")
+    sleep(1)
+    window_before = browser.window_handles[0]# Chama a primeira guia de "windows_before"
+    
     sleep(5)
     period = browser.find_element_by_xpath('/html/body/div[2]/div[1]/div[1]/div/div[2]/div/div[1]/div[2]')
     period.click()
@@ -70,14 +70,27 @@ async def preco_slp(preco_slp):
     
     ad = browser.find_element_by_xpath('/html/body/div[6]/div/span/div[1]/div/div/div[3]/div[2]/div/div/span[2]/span')
     ad.click()
+    
     sleep(5)
-
+    
     link = pyperclip.paste()
+    
+    browser.execute_script("window.open('{link}')") # Abre segunda guia
+    
+    sleep(1)
+    
+    window_after = browser.window_handles[1] # Chama a segunda guia de "windows_after"
 
-    imagem = link
+    browser.switch_to.window(window_after) # Troca para a segunda guia
+    
+    sleep(1)
+    
+    buttomGabarito = browser.get("https://pt.stackoverflow.com/questions/ask") # Acessa o link na segunda guia
+
+    
+
 
     print(link)
-    browser.quit()
 
     embed_slp = discord.Embed(
         title= 'Preço SLP',
@@ -90,7 +103,7 @@ async def preco_slp(preco_slp):
     embed_slp.set_author(name='SLP', icon_url='')
 
     embed_slp.set_image(
-        url=f'{imagem}'
+        url=f'{link}'
     )
     
     
