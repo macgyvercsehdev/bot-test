370afd0b37a328c998168870b0e35cf0a7dbd10a
macgyvercsehdev <macgyvercseh946@gmail.com>
2021-08-22 04:25:48 +0000

    fix

--- main.py
+++ main.py
@@ -47,6 +47,35 @@ async def ola(ola):
 async def preco_slp(preco_slp):
     result = float(slp_price['smooth-love-potion']['brl'])
 
+    options = webdriver.ChromeOptions()
+    options.add_argument("no-sandbox")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--disable-dev-shm-usage")
+    
+    
+    browser = webdriver.Chrome(options=options)
+
+    browser.get("https://www.tradingview.com/chart/?symbol=BITSTAMP%3ABTCUSD")
+
+    browser.maximize_window()
+
+    sleep(5)
+    period = browser.find_element_by_xpath('/html/body/div[2]/div[1]/div[1]/div/div[2]/div/div[1]/div[2]')
+    period.click()
+    
+    
+    period = browser.find_element_by_xpath('/html/body/div[2]/div[2]/div/div/div[1]/div/div/div/div/div/div[16]')
+    period.click()
+    ad = browser.find_element_by_xpath('/html/body/div[6]/div/span/div[1]/div/div/div[3]/div[2]/div/div/span[2]/span')
+    ad.click()
+    sleep(5)
+
+    link = pyperclip.paste()
+
+    print(link)
+    browser.quit()
+
     embed_slp = discord.Embed(
         title= 'Preço SLP',
         description= f'Atualmente: R$ {result}',
@@ -58,63 +87,13 @@ async def preco_slp(preco_slp):
     embed_slp.set_author(name='SLP', icon_url='')
 
     embed_slp.set_image(
-        url='https://s3.tradingview.com/snapshots/g/GTO7nBy7.png'
+        url=f'{link}'
     )
     
     embed_slp.set_footer(text="feito por Csehz#0527")
 
     await preco_slp.send(embed=embed_slp)
-
-
-@cliente.command()
-async def graf_pvu_5(graf_pvu_5):
-    
-    
-    useragent = "Mozilla/5.0 (Linux; Android 8.0.0; Pixel 2 XL Build/OPD1.170816.004) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Mobile Safari/537.36"
-
-    #Firefox
-    profile = webdriver.FirefoxProfile()
-    profile.set_preference("general.useragent.override", useragent)
-    options = webdriver.FirefoxOptions()
-    options.set_preference("dom.webnotifications.serviceworker.enabled", False)
-    options.set_preference("dom.webnotifications.enabled", False)
-    options.add_argument('--headless')
-    
-    
-    browser = webdriver.Firefox(firefox_profile=profile, options=options, executable_path='/usr/local/bin/geckodriver', firefox_binary='/usr/bin/firefox')
-
-    browser.get("https://www.tradingview.com/chart/?symbol=BITSTAMP%3ABTCUSD")
-
-    browser.maximize_window()
-
-    sleep(5)
-
-    period = browser.find_element_by_xpath('/html/body/div[2]/div[1]/div[1]/div/div[2]/div/div[1]/div[2]')
-    period.click()
-    
-    
-    period = browser.find_element_by_xpath('/html/body/div[2]/div[2]/div/div/div[1]/div/div/div/div/div/div[16]')
-    period.click()
-    sleep(2)
-    ad = browser.find_element_by_xpath('/html/body/div[6]/div/span/div[1]/div/div/div[3]/div[2]/div/div/span[2]/span')
-    ad.click()
-
-    sleep(2)
-    
-    link = pyperclip.paste()
-
-    embed_char_slp = discord.Embed(
-        title= 'Gráfico SLP: 5 dias',
-        description= link,
-        colour = discord.Colour.green()
-    )
-    embed_char_slp.set_footer(text="feito por Csehz#0527")
-
-    sleep(2)
-    await graf_pvu_5.send(embed=embed_char_slp)
-
-    print(link)
-    print('Screenshots taken')
+   
 
 
 
